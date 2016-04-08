1b0b9ede7beba2ca267cd87a4245f51e15a76db7
joebu23 <joe.royston@gmail.com>
2016-04-07 21:29:58 -0400

    update

--- SlackBot.js
+++ SlackBot.js
@@ -4,20 +4,24 @@ var Phrases = require('./phrases')
 // create the bot
 var bot = new SlackBot({
 	token: 'xoxb-33006979879-CNgRf5icgwZVf8PcKz1GP7Vy',
-	name: 'Jim The Bot'
+	name: 'Family-Bot'
 });
 
 bot.on('start', function() {
 	var params = {
-		icon_emoji: ':dog:'
+		icon_emoji: ':poop:'
 	};
 });
 
 bot.on('message', function(data) {
-	if(data.type == "message" && data.username != "Jim The Bot")
+	if(data.type == "message" && data.username != "Family-Bot")
 	{
+		if(data.text.split(':')[0] == "<@U0Z0^UTRV>")
+		{
+			bot.postMessageToChannel('family-stuff', "You never talk right to me", params);
+		}
 		console.log(data);
 		console.log('----------');
-		bot.postMessageToChannel('family-stuff', Phrases.getPhrase());
+		bot.postMessageToChannel('family-stuff', Phrases.getPhrase(), params);
 	}
 });
